e05970bca3651110eb85e726ec1a9775ec6ce95e
송명석 <sms4621875@naver.com>
2025-05-01 20:00:36 +0900

    Update example.test.js

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,3 +2,11 @@
 test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
 });
+
+test('intentional failure 1', () => {
+  expect(true).toBe(false);
+});
+
+test('intentional failure 2', () => {
+  expect(5).toBe(10);
+});
